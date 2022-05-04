ac2f08817f54094fb30356bd7f9c0555ade21e52
robotchiti <myemail@example.com>
2022-05-04 15:53:17 +0300

    added table widget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow,QTableWidgetItem,QPushButton
 
 from uis.UI_MainWindow import Ui_MainWindow
 
@@ -32,9 +32,29 @@ class AppLaunch():
 
     #click handlers
     def showItems(self):
+        self.editbtn = QPushButton()
+        self.editbtn.setText("edit")
+        self.ui.items_table.setRowCount(10)
+        self.ui.items_table.setColumnCount(8)
+
+
+        self.ui.items_table.setHorizontalHeaderLabels(["Name", "Job", "Email"])
+        self.ui.items_table.setItem(0,0,QTableWidgetItem('Name'))
+        self.ui.items_table.setItem(0,1,QTableWidgetItem('City'))
+        self.ui.items_table.setItem(0,2,QTableWidgetItem('Street'))
+
         self.ui.stackedWidget.setCurrentWidget(self.ui.items)
 
     def showSales(self):
+        data = {'col1':['1','2','3','4'],
+        'col2':['1','2','1','3'],
+        'col3':['1','1','2','1']}
+
+        self.ui.sales_table.setRowCount(10)
+        self.ui.sales_table.setColumnCount(8)
+
+        self.ui.sales_table.setItem(0,0,QTableWidgetItem('Name'))
+        self.ui.sales_table.setItem(0,1,QTableWidgetItem('City'))
         self.ui.stackedWidget.setCurrentWidget(self.ui.sales)
     
     def showCategories(self):
