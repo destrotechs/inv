faba419adaa8aa228d3b9046ed44838ff36ac7f5
robotchiti <myemail@example.com>
2022-05-04 12:40:07 +0300

    added menu pages

--- main.py
+++ main.py
@@ -14,9 +14,37 @@ class AppLaunch():
 
         #setup ui to the main window
         self.ui.setupUi(self.main_window)
+        #set default widget
+        self.ui.stackedWidget.setCurrentWidget(self.ui.home)
+
+        #listen for menu button clicks
+        self.ui.items_btn.clicked.connect(self.showItems)
+        self.ui.sales_btn.clicked.connect(self.showSales)
+        self.ui.categories_btn.clicked.connect(self.showCategories)
+        self.ui.stock_btn.clicked.connect(self.showStock)
+        self.ui.reports_btn.clicked.connect(self.showReports)
+
+        
 
     def showUi(self):
         self.main_window.show()
+    
+
+    #click handlers
+    def showItems(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.items)
+
+    def showSales(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.sales)
+    
+    def showCategories(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.categories)
+    
+    def showStock(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.stock)
+    
+    def showReports(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.reports)
 
 
 
